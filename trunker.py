0b07b4f018f3d252521a7a1d9876847efa8197b5
Jason Jones <jason.jones@blueglacier.com>
2020-09-23 11:03:20 -0400

    added Class variables for log_path, if_path (interface path), and if_path_orig (interface path orig)

--- trunker.py
+++ trunker.py
@@ -5,41 +5,44 @@ import subprocess
 import logging
 from IPy import IP
 
+
 class Trunker:
     def __init__(self, vdevices, interface):
         # class vairables
         self.vdevices = int(vdevices)
         self.interface = interface
+        self.log_path = "root/site-data/logs/"
+        self.if_path = "/etc/network/interfaces"
+        self.if_path_orig = if_path + ".orig"
 
     def log_creation(self):
-        if len(subprocess.Popen(["ls", "/root/site-data/logs/"], stdout = subprocess.PIPE, stderr = subprocess.PIPE).communicate()[1]) > 0:
+        if len(subprocess.Popen(["ls", self.log_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[1]) > 0:
             while True:
                 try:
-                    print("/root/site-data/logs/ does not exist")
+                    print(f"{self.log_path} does not exist")
                     log_file = input("Please enter a filepath for trunker.log: ")
-                    logging.basicConfig(filename = log_file + "/trunker.log", format="%(asctime)s %(message)s", level = logging.DEBUG, datefmt="%m/%d/%Y %H:%M:%S")
+                    logging.basicConfig(filename=log_file + "/trunker.log", format="%(asctime)s %(message)s", level=logging.DEBUG, datefmt="%m/%d/%Y %H:%M:%S")
                     return print(f"{log_file}/trunker.log created. Logging started")
                 except FileNotFoundError:
                     print(f"{log_file} does not exist. Enter another")
                     continue
         else:
-            logging.basicConfig(filename = "/root/site-data/logs/trunker.log", format="%(asctime)s %(message)s", level = logging.DEBUG, datefmt="%m/%d/%Y %H:%M:%S")
+            logging.basicConfig(filename=self.log_path + "trunker.log", format="%(asctime)s %(message)s", level=logging.DEBUG, datefmt="%m/%d/%Y %H:%M:%S")
 
     def interfaces_check(self):
         try:
-            if len(subprocess.Popen(["ls", "-a", "/etc/network/interfaces.orig"], stdout = subprocess.PIPE, stderr = subprocess.PIPE).communicate()[0]) > 0:
-                logging.info("/etc/network/interfaces.orig already exists")
-                logging.info("Appending devices to /etc/network/interfaces") 
+            if len(subprocess.Popen(["ls", "-a", self.if_path_orig], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]) > 0:
+                logging.info(f"{self.if_path_orig} already exists")
+                logging.info(f"Appending devices to {self.if_path}")
                 raise ValueError
             else:
-                subprocess.Popen(["cp", "-p", "/etc/network/interfaces", "/etc/network/interfaces.orig"]).communicate()
-                logging.info("/etc/network/interfaces.orig does not exist")
-                logging.info("Copied /etc/network/interfaces -> /etc/network/interfaces.orig")
-                print("/etc/network/interfaces found.  Creating backup at /etc/network/interfaces.orig")
+                subprocess.Popen(["cp", "-p", self.if_path, self.if_path_orig]).communicate()
+                logging.info(f"{self.if_path_orig} does not exist")
+                logging.info(f"Copied {self.if_path} -> {self.if_path_orig}")
+                print(f"{self.if_path} found.  Creating backup at {self.if_path_orig}")
         except ValueError:
-            print("/etc/network/interfaces.orig already exists")
-            print("Appending devices to /etc/network/interface")
-
+            print(f"{self.if_path_orig} already exists")
+            print(f"Appending devices to {self.if_path}")
 
     def add(self):
         self.log_creation()
@@ -87,17 +90,17 @@ class Trunker:
                                             device_ip,
                                             device_nm)
 
-            if vlan_device_name == False:
+            if not vlan_device_name:
                 return print("Virtual Device already exists. Try running ./trunker.py -r")
             else:
                 print(f"VLAN {vlan_device_name} created\nIP Address: {device_ip}/{device_cidr}")
                 logging.info(f"Virtual Device {vlan_device_name} created.  Assigned IP Address {device_ip}/{device_cidr}")
 
     def get_vlan_names(self):
-        get_vlan_names = subprocess.Popen(["ls", "-a", "/proc/net/vlan"], stdout = subprocess.PIPE).communicate()[0]
+        get_vlan_names = subprocess.Popen(["ls", "-a", "/proc/net/vlan"], stdout=subprocess.PIPE).communicate()[0]
         match_names = [s for s in get_vlan_names.decode("utf-8").split("\n") if "eth0." in s]
         return match_names
-        
+
     def delete(self):
         self.log_creation()
         match_names = self.get_vlan_names()
@@ -106,14 +109,14 @@ class Trunker:
             print(f"Deleted: {device}")
             logging.info(f"Deleted: {device}")
 
-        err = subprocess.Popen(["mv", "/etc/network/interfaces.orig", "/etc/network/interfaces"], stderr = subprocess.PIPE).communicate()[1]
-        logging.info("Moved /etc/network/interfaces.orig -> /etc/network/interfaces")
+        err = subprocess.Popen(["mv", self.if_path_orig, self.if_path], stderr=subprocess.PIPE).communicate()[1]
+        logging.info(f"Moved {self.if_path_orig} -> {self.if_path}")
 
     def from_file(self, input_file):
         self.log_creation()
         self.interfaces_check()
         for line in input_file:
-            vlan_device_id, device_ip, device_cidr = line.split(",")[0:3] 
+            vlan_device_id, device_ip, device_cidr = line.split(",")[0:3]
             device_cidr = device_cidr.strip()
 
             try:
@@ -139,11 +142,11 @@ class Trunker:
                 print("Run 'trunker -r' before trying again")
                 break
 
-            device_nm = self.cidr_to_netmask(int(device_cidr)) 
+            device_nm = self.cidr_to_netmask(int(device_cidr))
             vlan_device_name = self.vconfig(vlan_device_id,
-                                                device_ip,
-                                                device_nm)
-            if vlan_device_name == False:
+                                            device_ip,
+                                            device_nm)
+            if not vlan_device_name:
                 return print("Virtual Device already exists. Try running ./trunker.py -r")
             else:
                 print(f"VLAN {vlan_device_name} created\nIP Address: {device_ip}/{device_cidr}")
@@ -151,15 +154,15 @@ class Trunker:
                 logging.info(f"Assigned IP Address {device_ip}/{device_cidr}")
 
     def make_persist(self, vlan_device_name, device_ip, device_nm):
-        logging.info(f"Adding VLAN device {vlan_device_name} to /etc/network/interfaces")
-        with open("/etc/network/interfaces", "a+") as interfaces:
+        logging.info(f"Adding VLAN device {vlan_device_name} to {self.if_path}")
+        with open(self.if_path, "a+") as interfaces:
             interfaces.write("\n# VLAN device {} added by trunker\nauto {}\niface {} inet static\n\taddress {}\n\tnetmask {}\n\tvlan-raw-device {}\n".format(
-                        vlan_device_name,
-                        vlan_device_name,
-                        vlan_device_name,
-                        device_ip,
-                        device_nm,
-                        self.interface
+                vlan_device_name,
+                vlan_device_name,
+                vlan_device_name,
+                device_ip,
+                device_nm,
+                self.interface
             ))
 
     def cidr_to_netmask(self, device_cidr):
@@ -179,29 +182,30 @@ class Trunker:
         vlan_device_name = "%s.%s" % (self.interface, str(vlan_device_id))
         prefix_ip = "%s/%s" % (str(device_ip), str(device_nm))
         create_vlan_device = subprocess.Popen(["ip", "link", "add", "link", self.interface, "name", vlan_device_name,
-                                               "type", "vlan", "id", str(vlan_device_id)], stderr = subprocess.PIPE)
+                                               "type", "vlan", "id", str(vlan_device_id)], stderr=subprocess.PIPE)
 
         err = create_vlan_device.communicate()
         if len(err[1]) > 0:
             return False
 
         subprocess.Popen(["ip", "addr", "add", prefix_ip, "brd", str(device_nm),
-                                                 "dev", vlan_device_name])
+                          "dev", vlan_device_name])
         subprocess.Popen(["ip", "link", "set", vlan_device_name, "up"])
 
         self.make_persist(vlan_device_name, device_ip, device_nm)
 
         return vlan_device_name
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Use to add/remove VLAN ID to default 'eth0' interface",formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(description="Use to add/remove VLAN ID to default 'eth0' interface", formatter_class=argparse.RawDescriptionHelpFormatter)
     manual_group = parser.add_mutually_exclusive_group()
     manual_group.add_argument("-a", "--add", action="store_true", help="Add a single VLAN device. Combine with -n to create multiple")
     manual_group.add_argument("-r", "--rem", action="store_true", help="Remove all VLAN devices from /proc/net/vlan and /etc/network/interfaces")
     file_group = parser.add_mutually_exclusive_group()
-    file_group.add_argument("-n", "--num", type = int, default = 1, help = "Number of VLAN devices needed (default: 1)")
+    file_group.add_argument("-n", "--num", type=int, default=1, help="Number of VLAN devices needed (default: 1)")
     file_group.add_argument("-f", "--file", help="""Adds VLAN id's from a comma delimitted, multilined file.\nExample:\n1,192.168.1.1,24\n2,192.168.2.1,8""", type=argparse.FileType('r'))
-    parser.add_argument("-i", "--inter", default = "eth0", help = "Specify which interface to add VLAN to")
+    parser.add_argument("-i", "--inter", default="eth0", help="Specify which interface to add VLAN to")
     args = parser.parse_args()
 
     vlan_trunk = Trunker(args.num, args.inter)
@@ -215,6 +219,7 @@ def main():
     else:
         print("usage: trunker [-h] [-a ADD | -r REM | -f FILE] [-i INTER] [-n NUM]")
         print("-h, --help .... show help message:")
+
 
 if __name__ == "__main__":
     main()
